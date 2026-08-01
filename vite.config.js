71439eb48e27b1431fb207928a8e195ae4294aae
ainokori <eugene.aquila06@gmail.com>
2026-08-02 03:06:54 +0800

    Replace

--- vite.config.js
+++ vite.config.js
@@ -1,2 +1,8 @@
-import { defineConfig } from 'vite';
-export default defineConfig({});
+import { defineConfig } from 'vite'
+import tailwindcss from '@tailwindcss/vite'
+
+export default defineConfig({
+  plugins: [
+    tailwindcss(),
+  ],
+})
