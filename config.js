21c9b505fdb0e5690c4bece3fdf502811a5a2ac5
ainokori <eugene.aquila06@gmail.com>
2026-08-02 02:44:43 +0800

    Replace

--- config.js
+++ config.js
@@ -1,9 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
-import dotenv from "dotenv";
-
-dotenv.config();
 
 export const supabase = createClient(
-    process.env.https://xzoidkaoupxzntkjviov.supabase.co,
-    process.env.eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inh6b2lka2FvdXB4em50a2p2aW92Iiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc4NTMwOTkzMywiZXhwIjoyMTAwODg1OTMzfQ.epdQsktP05cS0QVv9kUlrVwngYzmYkfS01KQI45EyZM
+  "https://xzoidkaoupxzntkjviov.supabase.co",
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inh6b2lka2FvdXB4em50a2p2aW92Iiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc4NTMwOTkzMywiZXhwIjoyMTAwODg1OTMzfQ.epdQsktP05cS0QVv9kUlrVwngYzmYkfS01KQI45EyZM"
 );
